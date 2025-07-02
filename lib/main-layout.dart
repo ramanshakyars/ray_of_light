429108fff09fa23b6ebce37f2d4bb17c4e635b61
ramanshakyars <ramanshakyars12@gmail.com>
2025-07-02 13:02:03 +0530

    add side menu bar

--- lib/main-layout.dart
+++ lib/main-layout.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:go_router/go_router.dart';
 import 'package:rayoflite/routenames.dart';
 
@@ -14,10 +13,19 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('My App'),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
+        ),
+      ),
       drawer: Drawer(
         child: ListView(
           children: [
@@ -26,6 +34,35 @@ class _MainScreenState extends State<MainScreen> {
               child: Text('Menu'),
             ),
             ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                context.go('/${RouteNames.mainApp}/home');
+                setState(() => _currentIndex = 0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.search),
+              title: const Text('Search'),
+              onTap: () {
+                context.go('/${RouteNames.mainApp}/search');
+                setState(() => _currentIndex = 1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                context.go('/${RouteNames.mainApp}/profile');
+                setState(() => _currentIndex = 2);
+                Navigator.pop(context);
+              },
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.settings),
               title: const Text('Page 1'),
               onTap: () {
                 context.go('/${RouteNames.mainApp}/${RouteNames.page1}');
@@ -33,6 +70,7 @@ class _MainScreenState extends State<MainScreen> {
               },
             ),
             ListTile(
+              leading: const Icon(Icons.info),
               title: const Text('Page 2'),
               onTap: () {
                 context.go('/${RouteNames.mainApp}/${RouteNames.page2}');
@@ -51,13 +89,13 @@ class _MainScreenState extends State<MainScreen> {
           });
           switch (index) {
             case 0:
-              context.go('${RouteNames.mainApp}/home');
+              context.go('/${RouteNames.mainApp}/home');
               break;
             case 1:
-              context.go('${RouteNames.mainApp}/search');
+              context.go('/${RouteNames.mainApp}/search');
               break;
             case 2:
-              context.go('${RouteNames.mainApp}/profile');
+              context.go('/${RouteNames.mainApp}/profile');
               break;
           }
         },
