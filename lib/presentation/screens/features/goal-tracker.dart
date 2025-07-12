d022415c6bc5712460dd913ba0a89aa48dd63b90
ramanshakyars <ramanshakyars12@gmail.com>
2025-07-13 02:33:56 +0530

    working on goal tracker

--- lib/presentation/screens/features/goal-tracker.dart
+++ lib/presentation/screens/features/goal-tracker.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:rayoflite/core/config/routenames.dart';
 
 class GoalTrackerExcerises extends StatelessWidget {
   const GoalTrackerExcerises({super.key});
@@ -7,12 +9,26 @@ class GoalTrackerExcerises extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Talk to Lite'),
-        centerTitle: true,
+        title: Text('Track Your Goals', style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0))),
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        elevation: 10,
+        automaticallyImplyLeading: false,
+        // leading: IconButton(
+        //   icon: Icon(Icons.menu, color: const Color.fromARGB(255, 0, 0, 0)),
+        //   onPressed: () {},
+        // ),
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/logo.png'),
+            onPressed: () {
+              GoRouter.of(
+                context,
+              ).push('${RouteNames.mainApp}/${RouteNames.home}');
+            },
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text(''),
-      ),
+      body: const Center(child: Text('')),
     );
   }
 }
