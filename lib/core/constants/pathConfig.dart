4e736547bc85ee45054bfa4bad71ad4f891c4068
ramanshakyars <ramanshakyars12@gmail.com>
2025-08-19 02:03:29 +0530

    working on production code

--- lib/core/constants/pathConfig.dart
+++ lib/core/constants/pathConfig.dart
@@ -1,6 +1,6 @@
 class PathConfig {
-  static const String baseUrl = 'http://localhost:9090/rayoflight/api';
-  // static const String baseUrl = 'http://ec2-3-111-209-210.ap-south-1.compute.amazonaws.com/rayoflight/api';
+  // static const String baseUrl = 'http://localhost:9090/rayoflight/api';
+  static const String baseUrl = 'http://ec2-3-111-209-210.ap-south-1.compute.amazonaws.com/rayoflight/api';
 
   // Auth endpoints
   static const String login = '$baseUrl/public/login';
